abbd2359545ba24e2e55794b4cd3dd919d7d100f
Jacob Johnson <jacob.k.johnson@utah.edu>
2023-10-23 13:26:37 -0600

    added script.sh

--- proof-of-concept.py
+++ proof-of-concept.py
@@ -12,8 +12,13 @@ processor = WhisperProcessor.from_pretrained("openai/whisper-large")
 model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large")
 model.config.forced_decoder_ids = None
 
+ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+sample = ds[0]["audio"]
 
 #Make spectrogram ?
 input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
 
-print(input_features)
+predicted_ids = model.generate(input_features)
+transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+print(transcription)
---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account soc-gpu-np
+#SBATCH --partition soc-gpu-np
+#SBATCH --ntasks-per-node=32
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=1:00:00
+#SBATCH --mem=40GB
+#SBATCH --mail-user=jacob.k.johnson@utah.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH -o assignment_1-%j
+
+conda create -n salASR python=3.8
+pip install -r requirements.txt
+conda activate salASR
+
+python proof-of-concept.py
