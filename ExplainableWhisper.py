a42a3569039499863ed5311151b54bb95b83bdce
Jacob Johnson <jacob.k.johnson@utah.edu>
2023-10-25 13:54:29 -0600

    added top_r_features() -- seems to be working so far

--- ExplainableWhisper.py
+++ ExplainableWhisper.py
@@ -1,4 +1,5 @@
 from transformers import WhisperForConditionalGeneration
+from transformers.models.whisper import WhisperConfig
 
 from transformers.generation.utils import *
 
@@ -7,14 +8,22 @@ def get_saliency_callback(inputs: torch.Tensor):
 
     saliency_list = []
     def saliency_callback_fn(logit: torch.Tensor):
+        
+        print("LOGIT REQUIRES GRAD?", logit.requires_grad)
+        
         logit.backward(retain_graph=True)
         saliency_map = inputs.grad
-        inputs.grad.zero_()
         saliency_list.append(saliency_map)
+        inputs.grad.zero_()
 
     return saliency_list, saliency_callback_fn
 
 class ExplainableWhisper(WhisperForConditionalGeneration):
+    def __init__(self, config: WhisperConfig):
+        super().__init__(config)
+        self.requires_grad_(True)
+
+
     def generate(
         self,
         inputs: Optional[torch.Tensor] = None,
@@ -295,3 +304,103 @@ class ExplainableWhisper(WhisperForConditionalGeneration):
                 )
         else:
             return input_ids
+        
+    def forward(
+        self,
+        input_features: Optional[torch.FloatTensor] = None,
+        attention_mask: Optional[torch.LongTensor] = None,
+        decoder_input_ids: Optional[torch.LongTensor] = None,
+        decoder_attention_mask: Optional[torch.LongTensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        decoder_head_mask: Optional[torch.Tensor] = None,
+        cross_attn_head_mask: Optional[torch.Tensor] = None,
+        encoder_outputs: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
+        past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
+        decoder_inputs_embeds: Optional[Tuple[torch.FloatTensor]] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], Seq2SeqLMOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Labels for computing the language modeling loss. Indices should either be in `[0, ..., config.vocab_size]`
+            or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored (masked), the loss is
+            only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+        Returns:
+
+        Example:
+
+        ```python
+        >>> import torch
+        >>> from transformers import AutoProcessor, WhisperForConditionalGeneration
+        >>> from datasets import load_dataset
+
+        >>> processor = AutoProcessor.from_pretrained("openai/whisper-tiny.en")
+        >>> model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny.en")
+
+        >>> ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+
+        >>> inputs = processor(ds[0]["audio"]["array"], return_tensors="pt")
+        >>> input_features = inputs.input_features
+
+        >>> generated_ids = model.generate(inputs=input_features)
+
+        >>> transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        >>> transcription
+        ' Mr. Quilter is the apostle of the middle classes, and we are glad to welcome his gospel.'
+        ```"""
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        print("RUNNING FORWARD PASS") # # # # #
+        if labels is not None:
+            print("labels is NONE") # # # # #
+            if decoder_input_ids is None and decoder_inputs_embeds is None:
+                print("d_i_d is NONE and d_i_e is NONE")
+                from transformers.models.whisper import shift_tokens_right
+                decoder_input_ids = shift_tokens_right(
+                    labels, self.config.pad_token_id, self.config.decoder_start_token_id
+                )
+
+        outputs = self.model(
+            input_features,
+            attention_mask=attention_mask,
+            decoder_input_ids=decoder_input_ids,
+            encoder_outputs=encoder_outputs,
+            decoder_attention_mask=decoder_attention_mask,
+            head_mask=head_mask,
+            decoder_head_mask=decoder_head_mask,
+            cross_attn_head_mask=cross_attn_head_mask,
+            past_key_values=past_key_values,
+            decoder_inputs_embeds=decoder_inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        lm_logits = self.proj_out(outputs[0])
+
+        loss = None
+        if labels is not None:
+            from torch.nn import CrossEntropyLoss
+            loss_fct = CrossEntropyLoss()
+            # move labels to correct device to enable PP
+            labels = labels.to(lm_logits.device)
+            loss = loss_fct(lm_logits.view(-1, self.config.vocab_size), labels.reshape(-1))
+
+        if not return_dict:
+            output = (lm_logits,) + outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return Seq2SeqLMOutput(
+            loss=loss,
+            logits=lm_logits,
+            past_key_values=outputs.past_key_values,
+            decoder_hidden_states=outputs.decoder_hidden_states,
+            decoder_attentions=outputs.decoder_attentions,
+            cross_attentions=outputs.cross_attentions,
+            encoder_last_hidden_state=outputs.encoder_last_hidden_state,
+            encoder_hidden_states=outputs.encoder_hidden_states,
+            encoder_attentions=outputs.encoder_attentions,
+        )
--- proof-of-concept.py
+++ proof-of-concept.py
@@ -3,26 +3,65 @@ import argparse, logging
 import transformers
 
 
-
+import torch
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from datasets import load_dataset
 
 from ExplainableWhisper import ExplainableWhisper # # # 
 
-processor = WhisperProcessor.from_pretrained("openai/whisper-large")
-model = ExplainableWhisper.from_pretrained("openai/whisper-large") # # # model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large")
+processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+model = ExplainableWhisper.from_pretrained("openai/whisper-tiny") # # # model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-large")
 model.config.forced_decoder_ids = None
 
 ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-sample = ds[0]["audio"]
+#sample = ds[0]["audio"]
 
 #Make spectrogram ?
-input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
+#input_features = processor(sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt").input_features 
 
-input_features.requires_grad = True
+# # # # # predicted_ids = model.generate(input_features)
+#model_output = model.forward(input_features, decoder_input_ids=torch.LongTensor([[50258, 1, 2, 3]]))
+
+#logits = model_output.logits
+# transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+# print(transcription)
 
 
-predicted_ids = model.generate(input_features)
-transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
 
-print(transcription)
+#load_dataset("mozilla-foundation/common_voice_13_0", "en", split="train[:1%]+validation[:1%]+test[:1%]")
+
+def build_saliency_mask(saliency: torch.Tensor, r=.5, k=None):
+    if k is None:
+        k = int(r * saliency.numel())
+    saliency_abs = saliency.abs()
+    return (saliency_abs >= saliency_abs.flatten().topk(k).values.min())
+
+
+
+def build_saliency_map(sample):
+    input_features = processor(sample['audio']["array"], sampling_rate=sample['audio']["sampling_rate"], return_tensors="pt").input_features
+
+    input_features.requires_grad = True
+
+    sample['input_features'] = input_features
+
+    decoder_input_ids = torch.LongTensor([processor.tokenizer(sample['text']).input_ids])
+
+    model_output = model.forward(input_features, decoder_input_ids=decoder_input_ids)
+    logits = model_output.logits[0]
+
+    total = logits.gather(1, logits.argmax(dim=1)[None].T).sum()
+
+    total.backward(retain_graph=True)
+
+    return input_features.grad
+
+
+def top_r_features(sample):
+    saliency_map = build_saliency_map(sample)
+
+    saliency_mask = build_saliency_mask(saliency_map)
+
+    return sample['input_features'] * saliency_mask
+
