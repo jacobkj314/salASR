a25f7a8cd8820d6e4f54c507bbfeb0a41c2c1c16
Gurunath <gurunathpr@gmail.com>
2023-11-01 08:54:04 -0600

    Include tqdm for iterator

--- main.py
+++ main.py
@@ -27,11 +27,13 @@ def main(args):
     output_dir = Path(args.output_dir)
     r_value = args.r_value
     #load processor and model
+    print(f"Loading model . . . ")
     whisper_evaluator = EvalWhisper(f"openai/whisper-{model_size}")
+    print(f"Loaded model")
     ds = load_dataset("librispeech_asr", split="validation.clean", streaming=True)
     print(f"Loaded data")
     scores_list = []
-    for sample in ds.take(num_samples):
+    for sample in tqdm(ds.take(num_samples)):
         scores_list.append(whisper_evaluator.evaluate(sample, whisper_evaluator.top_r_features(sample, r=r_value)))
     print(f"scores_list:{scores_list}")
     output_path = output_dir / "output.json"
