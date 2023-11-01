436cb87a96d100aa499094336ed2c5aa1cf4ee6b
Gurunath <gurunathpr@gmail.com>
2023-11-01 03:38:58 -0600

    Add thresholding args

--- main.py
+++ main.py
@@ -24,13 +24,14 @@ def main(args):
     num_samples = args.num_samples
     model_size = args.model_size
     output_dir = Path(args.output_dir)
+    r_value = args.r_value
     #load processor and model
     whisper_evaluator = EvalWhisper(f"openai/whisper-{model_size}")
     ds = load_dataset("librispeech_asr", split="validation.clean", streaming=True)
     print(f"Loaded data")
     scores_list = []
     for sample in ds.take(num_samples):
-        scores_list.append(whisper_evaluator.evaluate(sample, whisper_evaluator.top_r_features(sample, r=0.7)))
+        scores_list.append(whisper_evaluator.evaluate(sample, whisper_evaluator.top_r_features(sample, r=r_value)))
     print(f"scores_list:{scores_list}")
     output_path = output_dir / "output.json"
     mean, standard_deviation = calculate_stats(scores_list)
@@ -43,5 +44,6 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--num_samples", type=int, default=10)
     parser.add_argument("-m", "--model_size", type=str, default="tiny")
     parser.add_argument("-o", "--output_dir", type=str, default="./")
+    parser.add_argument("-r", "--r_value", type=int, default=1.0)
     args = parser.parse_args()
     main(args)
